a002e4f0675b15a484102f76e506f1add78a104f
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2021-11-06 13:53:45 -0700

    Create StepsUpdaterJob to listen to steps count updates.

---
+++ app/src/main/kotlin/dev/sjaramillo/pedometer/service/StepsUpdaterJob.kt
@@ -0,0 +1,50 @@
+package dev.sjaramillo.pedometer.service
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import dev.sjaramillo.pedometer.data.PedometerDatabase
+import dev.sjaramillo.pedometer.data.StepsRepository
+import dev.sjaramillo.pedometer.util.Logger.log
+
+/**
+ * Attempts to obtain a reading from the device's step sensor to update the app's
+ * step count.
+ */
+class StepsUpdaterJob: JobService(), SensorEventListener {
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("StepsUpdaterJob onJobStart()")
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)?.let { sensor ->
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+            return true
+        }
+
+        return false
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        return true
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        log("onSensorChanged: sensorEvent: $event")
+        event?.values?.firstOrNull()?.let { steps ->
+            log("Step count: $steps ")
+            val stepsRepository = StepsRepository(PedometerDatabase.getInstance(this))
+            stepsRepository.updateStepsSinceBoot(steps.toLong())
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // nobody knows what happens here: step value might magically decrease
+        // when this method is called...
+        log(sensor?.name + " accuracy changed: " + accuracy)
+    }
+}
