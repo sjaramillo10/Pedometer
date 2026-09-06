31be6e65950c637e85cc5b618a6c347608e14cdb
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2026-09-05 17:47:47 -0700

    feat: host stats screen directly in compose

--- app/src/main/kotlin/dev/sjaramillo/pedometer/ui/stats/StatsFragment.kt
+++ app/src/main/kotlin/dev/sjaramillo/pedometer/ui/stats/StatsFragment.kt
@@ -5,26 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.google.android.material.composethemeadapter.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
-import dev.sjaramillo.pedometer.R
 
 @AndroidEntryPoint
 class StatsFragment : Fragment() {
+    private val statsViewModel: StatsViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_stats, container, false)
-
-        view.findViewById<ComposeView>(R.id.compose_view).setContent {
-            MdcTheme {
-                StatsScreen()
+    ): View =
+        ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MdcTheme {
+                    StatsScreen(statsViewModel)
+                }
             }
         }
-
-        return view
-    }
 }
